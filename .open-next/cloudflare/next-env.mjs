28bca43c7fde6adc5bf78dfea4b829bc62baa27a
berto2004 <141506703+berto2004@users.noreply.github.com>
2026-09-02 10:36:04 +0700

    Clean build output from repository

--- .open-next/cloudflare/next-env.mjs
+++
@@ -1,3 +0,0 @@
-export const production = {"BETTER_AUTH_SECRET":"detailflow-local-dev-secret-change-before-production-2026","BETTER_AUTH_URL":"http://localhost:3000"};
-export const development = {"BETTER_AUTH_SECRET":"detailflow-local-dev-secret-change-before-production-2026","BETTER_AUTH_URL":"http://localhost:3000"};
-export const test = {};
